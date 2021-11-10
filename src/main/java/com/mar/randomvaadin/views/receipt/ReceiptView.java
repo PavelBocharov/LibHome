dc8880ff8137a0357f9f0d452ca5641817fb2b32
marolok <pbcharv@gmail.com>
2021-11-10 21:01:53 +0300

    CRUD receipt.

--- src/main/java/com/mar/randomvaadin/views/receipt/ReceiptView.java
+++ src/main/java/com/mar/randomvaadin/views/receipt/ReceiptView.java
@@ -1,21 +1,25 @@
 package com.mar.randomvaadin.views.receipt;
 
-import com.google.common.collect.ImmutableList;
-import com.mar.randomvaadin.db.entity.Product;
 import com.mar.randomvaadin.db.entity.Receipt;
+import com.mar.randomvaadin.utils.DeleteDialogWidget;
 import com.mar.randomvaadin.views.MainView;
 import com.mar.randomvaadin.views.product.ProductViewDialog;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Date;
+import java.time.ZoneId;
 import java.util.List;
-import java.util.Random;
+
+import static com.vaadin.flow.component.button.ButtonVariant.LUMO_TERTIARY;
 
 @RequiredArgsConstructor
 public class ReceiptView {
@@ -24,33 +28,59 @@ public class ReceiptView {
 
     public VerticalLayout getContent() {
         VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSizeFull();
 
-        Random random = new Random();
-        List<Receipt> receipts = ImmutableList.of(
-                Receipt.builder().product(Product.builder().name("Fish").build()).date(new Date()).price(random.nextFloat()).build(),
-                Receipt.builder().product(Product.builder().name("Fish").build()).date(new Date()).price(random.nextFloat()).build(),
-                Receipt.builder().product(Product.builder().name("Fish").build()).date(new Date()).price(random.nextFloat()).build(),
-                Receipt.builder().product(Product.builder().name("Fish").build()).date(new Date()).price(random.nextFloat()).build()
-        );
+        H2 label = new H2("Чё, сколько и зачем потратил");
 
-        Grid<Receipt> grid = new Grid<>(3);
+        List<Receipt> receipts = appLayout.getRepositoryService().getReceiptRepository().findAll();
+
+        Grid<Receipt> grid = new Grid<>();
         grid.addColumn(receipt -> receipt.getProduct().getName()).setHeader("Product");
         grid.addColumn(Receipt::getPrice).setSortable(true).setHeader("Price");
-        grid.addColumn(Receipt::getDate).setHeader("Date");
-
         grid.setItems(receipts);
+        grid.setWidthFull();
+        grid.addThemeVariants(GridVariant.LUMO_COMPACT, GridVariant.LUMO_WRAP_CELL_CONTENT);
 
-        Button test = new Button(new Icon(VaadinIcon.MOBILE));
-        test.addClickListener(click -> {
-            new ProductViewDialog(appLayout);
+        grid.addComponentColumn(receipt -> {
+                    DatePicker date = new DatePicker(
+                            receipt.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                    date.setEnabled(false);
+                    return date;
+                }
+        ).setHeader("Date");
+        grid.addComponentColumn(receipt -> {
+            Button edtBtn = new Button(new Icon(VaadinIcon.PENCIL), clk -> {
+                new UpdateReceiptDialog(appLayout, receipt);
+            });
+            edtBtn.addThemeVariants(LUMO_TERTIARY);
+            Button dltBtn = new Button(new Icon(VaadinIcon.BAN), clk -> {
+                new DeleteDialogWidget(() -> {
+                    appLayout.getRepositoryService().getReceiptRepository().delete(receipt);
+                    appLayout.setContent(appLayout.getReceiptView().getContent());
+                });
+            });
+            dltBtn.addThemeVariants(LUMO_TERTIARY);
+            dltBtn.getStyle().set("color", "red");
+            return new HorizontalLayout(edtBtn, dltBtn);
         });
 
-        verticalLayout.add(
-                grid,
-                new HorizontalLayout(
-                        test
-                )
+        Button crtBtn = new Button(
+                "Создать запись",
+                new Icon(VaadinIcon.PLUS),
+                click -> new CreateReceiptDialog(appLayout)
         );
+        crtBtn.setWidthFull();
+
+        HorizontalLayout btns = new HorizontalLayout(
+                crtBtn,
+                new Button(new Icon(VaadinIcon.COG), click -> new ProductViewDialog(appLayout))
+        );
+
+        verticalLayout.setHorizontalComponentAlignment(FlexComponent.Alignment.START, label);
+        verticalLayout.setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER, grid);
+        verticalLayout.setHorizontalComponentAlignment(FlexComponent.Alignment.END, btns);
+
+        verticalLayout.add(label, grid, btns);
         return verticalLayout;
     }
 }
---
+++ src/main/java/com/mar/randomvaadin/views/receipt/UpdateReceiptDialog.java
@@ -0,0 +1,111 @@
+package com.mar.randomvaadin.views.receipt;
+
+import com.mar.randomvaadin.db.entity.Product;
+import com.mar.randomvaadin.db.entity.Receipt;
+import com.mar.randomvaadin.utils.ViewUtils;
+import com.mar.randomvaadin.views.MainView;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.BigDecimalField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.data.provider.ListDataProvider;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+public class UpdateReceiptDialog {
+
+    public UpdateReceiptDialog(MainView mainView, Receipt receipt) {
+        Dialog updateDialog = new Dialog();
+        updateDialog.setCloseOnEsc(true);
+        updateDialog.setCloseOnOutsideClick(false);
+
+        List<Product> products = mainView.getRepositoryService().getProductRepository().findAll();
+
+        TextField idField = new TextField();
+        idField.setWidthFull();
+        idField.setAutofocus(false);
+        idField.setEnabled(false);
+        idField.setLabel("ID");
+        idField.setValue(String.valueOf(receipt.getId()));
+
+        Select<Product> productSelect = new Select<Product>();
+        productSelect.setLabel("Продукт");
+        productSelect.setPlaceholder("Выберите продукт...");
+        productSelect.setTextRenderer(product -> product.getName());
+        productSelect.setDataProvider(new ListDataProvider<>(products));
+        productSelect.setWidthFull();
+        productSelect.setValue(receipt.getProduct());
+
+        BigDecimalField numberField = new BigDecimalField();
+        numberField.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT);
+        numberField.setLabel("Цена");
+        numberField.setWidthFull();
+        numberField.setValue(BigDecimal.valueOf(receipt.getPrice()));
+
+        DatePicker datePicker = new DatePicker("Дата покупки", LocalDate.now());
+        datePicker.setWidthFull();
+        datePicker.setValue(receipt.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+
+        Button uptBtn = new Button("Обновить", new Icon(VaadinIcon.PLUS));
+        uptBtn.addClickListener(click -> {
+            try {
+                receipt.setProduct(getProduct(productSelect));
+                receipt.setDate(Date.from(
+                        datePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()
+                ));
+                receipt.setPrice(getPrice(numberField));
+                mainView.getRepositoryService().getReceiptRepository().save(receipt);
+            } catch (Exception ex) {
+                ViewUtils.showErrorMsg("При обновлении произошла ошибка", ex);
+                uptBtn.setEnabled(true);
+                return;
+            }
+            mainView.setContent(mainView.getReceiptView().getContent());
+            updateDialog.close();
+        });
+        uptBtn.setWidthFull();
+        uptBtn.setDisableOnClick(true);
+        uptBtn.addClickShortcut(Key.ENTER);
+
+        updateDialog.add(
+                new Label("Обновить запись"),
+                idField,
+                productSelect,
+                numberField,
+                datePicker,
+                new HorizontalLayout(uptBtn, ViewUtils.getCloseButton(updateDialog))
+        );
+        updateDialog.open();
+    }
+
+    private Product getProduct(Select<Product> productSelect) {
+        Product product = productSelect.getValue();
+        if (product == null) {
+            productSelect.setErrorMessage("Выберите продукт");
+            throw new IllegalArgumentException("Не выбран продукт");
+        }
+        return product;
+    }
+
+    private float getPrice(BigDecimalField numberField) {
+        BigDecimal price = numberField.getValue();
+        if (price == null) {
+            numberField.setErrorMessage("Введите корректную цену");
+            throw new IllegalArgumentException("Не введена корректная цена");
+        }
+        return price.floatValue();
+    }
+
+}
