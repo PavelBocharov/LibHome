1e303e5600cebf4473379ff3c35b7c9042dda807
Pavel <Pavel_Bocharov@epam.com>
2025-10-19 10:38:35 +0300

    LibHomeDB - WebFlux to MVC.

--- LibHomeDB/src/main/java/com/mar/libhome/db/api/CardTypeApi.java
+++ LibHomeDB/src/main/java/com/mar/libhome/db/api/CardTypeApi.java
@@ -1,9 +1,8 @@
 package com.mar.libhome.db.api;
 
+import com.mar.libhome.db.aop.ApiLog;
 import com.mar.libhome.db.mongo.service.CardTypeService;
 import com.mar.libhome.dto.CardTypeDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,43 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/card/type")
 public class CardTypeApi {
 
-    private final Logger log = LoggerFactory.getLogger(CardTypeApi.class);
-
     @Autowired
     private CardTypeService cardTypeService;
 
+    @ApiLog
     @GetMapping
-    public Mono<List<CardTypeDto>> getAll() {
-        log.debug(">> get all card type");
-        return cardTypeService.getAll()
-                .doOnSuccess(list -> log.debug("<< get all card type size: {}", Optional.ofNullable(list).orElse(Collections.emptyList()).size()))
-                .doOnError(throwable -> log.error("!!! get all card type", throwable));
+    public List<CardTypeDto> getAllCardType() {
+        return cardTypeService.getAll();
     }
 
+    @ApiLog
     @PostMapping
-    public Mono<List<CardTypeDto>> save(@RequestBody List<CardTypeDto> dtoList) {
-        log.debug(">> save card type: {}", dtoList);
-        return cardTypeService.save(dtoList)
-                .doOnSuccess(type -> log.debug("<< save card type: {}", type))
-                .doOnError(throwable -> log.error("!!! save card type: {}", dtoList, throwable));
+    public List<CardTypeDto> saveCardTypeList(@RequestBody List<CardTypeDto> dtoList) {
+        return cardTypeService.save(dtoList);
     }
 
+    @ApiLog
     @DeleteMapping
-    public Mono<CardTypeDto> delete(@RequestBody CardTypeDto dto) {
-        log.debug(">> delete card type: {}", dto);
-        return cardTypeService.deleteById(dto.getId())
-                .doOnSuccess(type -> log.debug("<< delete card type: {}", type))
-                .doOnError(throwable -> log.error("!!! delete card type: {}", dto, throwable));
+    public CardTypeDto deleteCardType(@RequestBody CardTypeDto dto) {
+        return cardTypeService.deleteById(dto.getId());
     }
 
 }
